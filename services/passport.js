d1ebef5cc02777b339f9ed52bcc3bdcb23631c8f
George Nguyen <g.nguyen92@yahoo.com>
2017-08-01 14:52:15 -0700

    Add method to prevent the same googleID from creating new model instances in mongoose/mongoDB

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+// User is our Mongoose Model class, that holds model instances
 // mongoose.model is called with one arg, we are getting a model/schema
 const User =  mongoose.model('users');
 
@@ -13,8 +14,19 @@ passport.use(
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback'
 		},
+		// User.findOne searches for a User whos googleID matches a profile.id
+			// User.findOne returns a JS Promise, which is used in JS to handle async code
+			// the model class searchedUser is used to find out if a matching user was found
+			// if searchedUser is found, do nothing.  else create a new user
 		(accessToken, refreshToken, profile, done) => {
-			new User({ googleID: profile.id }).save();
+			User.findOne({ googleID: profile.id }).then((searchedUser) => {
+				if (searchedUser){
+					// the user matches an existing id, do nothing
+				} else {
+					// usser not found.  create new model instance
+					new User({ googleID: profile.id }).save();
+				}
+			})
 		}
 	)
 );
@@ -35,19 +47,6 @@ const keys = require('../config/keys');
   // , enter same project name, web app, js origin: http://localhost/3000, ...
   // , redirect: http://localhost/3000/*, save client ID and secret in config/keys.js and gitignore
 
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken: ', accessToken);
-			console.log('refreshToken: ', refreshToken);
-			console.log('profile: ', profile);
-		}
-	)
-);
+
 
 */
