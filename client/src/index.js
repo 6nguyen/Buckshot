07929f72fbdeda2f4b353e56c8bb21a65f5a55b0
George Nguyen <g.nguyen92@yahoo.com>
2017-08-30 12:18:27 -0700

    Create redux authorization reducer and import into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import App from './components/App';
 
+import App from './components/App';
+import reducers from './reducers';
+
+// create a Redux Store (like a data storage)
 const store = createStore( () => [], applyMiddleware() );
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+// hook up the Redux Store to our react app using the Provider tag
+// Provider is a react component that reads changes from redux store
+  // whenever store gets new state produced inside of it, Provider tells
+  // all children components (App and all its children) there's a state change.
+  // Provider will update all children components with the new state.
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>
+  , document.querySelector("#root"));
