acc3d389d7d9ce3d39be62de145d8e4ef8c4b1bd
George Nguyen <g.nguyen92@yahoo.com>
2017-08-30 12:51:20 -0700

    Set up react Router to correctly dispaly Header component, Dashboard, SurveyNew, LandingPage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,17 +3,23 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
-const SurveryNew = () => <h2>Survery New</h2>
+const SurveyNew = () => <h2>Survey New</h2>
 const LandingPage = () => <h2>Landing Page</h2>
 
 const App = () => {
   return (
     <div>
-    // BrowserRouter can only have ONE child, so put everything in a div
+    {/* BrowserRouter can only have ONE child, so put everything in a div */}
       <BrowserRouter>
         <div>
-          // path="/" specifies the root route.  Relative path, no domain name included
-          <Route path="/" component ={LandingPage} />
+          {/* path="/" specifies the root route.  Relative path, no domain name included */}
+          {/* Route will load ALL components that are found in the path, unless specified with
+            'exact'.
+            Ex without exact: "/surveys" path will also load component with "/" path */}
+          <Header /> {/* Always appears no matter what */}
+          <Route exact={true} path="/" component ={LandingPage} />
+          <Route exact path="/surveys" component ={Dashboard} />
+          <Route path="/surveys/new" component ={SurveyNew} />
         </div>
       </BrowserRouter>
     </div>
