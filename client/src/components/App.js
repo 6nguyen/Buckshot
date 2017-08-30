79f4efe4c66091186093155ac178beddabc730af
George Nguyen <g.nguyen92@yahoo.com>
2017-08-30 12:34:24 -0700

    Import BrowserRouter, Route into App.js, Create components and set LandingPage default component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveryNew = () => <h2>Survery New</h2>
+const LandingPage = () => <h2>Landing Page</h2>
 
 const App = () => {
   return (
     <div>
-      Yoooo!
+    // BrowserRouter can only have ONE child, so put everything in a div
+      <BrowserRouter>
+        <div>
+          // path="/" specifies the root route.  Relative path, no domain name included
+          <Route path="/" component ={LandingPage} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,6 @@
+// Records whether a user is logged in or not
+  // used to show user dashboard components etc using react router
+
 export default function(state = {}, action) {
   switch(action.type) {
     default:
