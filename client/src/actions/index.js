d8abe481d0320a72e46ff5d58508483624c231a0
George Nguyen <g.nguyen92@yahoo.com>
2017-08-30 21:23:48 -0700

    Bug Fix: package.json proxy syntax error. Refactor fetchUser() in src/actions/index.js

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,8 +3,8 @@ import { FETCH_USER } from './types';
 
 
 export const fetchUser = () => {
-  return function(dispatch) {
-    axios.get('api/current_user')
-      .then(res => dispatch({ type:FETCH_USER, payload: res }))
+  return async function(dispatch) {
+    const res = await axios.get('/api/current_user')
+        dispatch({ type:FETCH_USER, payload: res });
   };
 };
